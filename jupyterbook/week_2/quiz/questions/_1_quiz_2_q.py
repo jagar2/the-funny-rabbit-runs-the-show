721fa286bdaffd02b9d396b00a914870c80ca30c
jagar2 <jca92@drexel.edu>
2025-01-11 21:58:09 -0500

    Auto Update book #build

--- jupyterbook/week_2/quiz/questions/_1_quiz_2_q.py
+++ jupyterbook/week_2/quiz/questions/_1_quiz_2_q.py
@@ -13,9 +13,9 @@ class Question1(MCQuestion):
             style=MCQ,
             question_number=1,
             keys=['q1-1-List-Immutability', 'q1-2-String-Operations'],
-            options=[['- String', '- Tuple', '- List', '- Integer'], ['- Hello + 5', '- Hello5', '- Error', '- Hello 5']],
+            options=[['String', 'Tuple', 'List', 'Integer'], ['Hello + 5', 'Hello5', 'Error', 'Hello 5']],
             descriptions=['Which of the following data structures in Python is mutable?', 'What would be the output of the following code: `"Hello" + str(5)`?'],
-            points=[2.0, 2.0],
+            points=[1.0, 1.0],
         )
 class Question3(TFQuestion):
     def __init__(self):
@@ -25,7 +25,7 @@ class Question3(TFQuestion):
             question_number=3,
             keys=['q3-1-List-Slicing', 'q3-2-Dictionary-Keys'],
             descriptions=['In Python, the slice operation `my_list[1:4]` includes the element at index 4.', 'Dictionary keys in Python must be unique within the same dictionary.'],
-            points=[2.0, 2.0],
+            points=[1.0, 1.0],
         )
 class Question2(SelectMany):
     def __init__(self):
@@ -36,6 +36,6 @@ class Question2(SelectMany):
             keys=['q2-1-Dictionary-Operations', 'q2-2-**String Basics**'],
             descriptions=['Which of the following operations are valid for Python dictionaries?', 'Which of the following string methods can change the case of characters in a string?'],
             options=[['Using a list as a dictionary key', 'Adding new key-value pairs after creation', 'Having multiple values for the same key', 'Using numbers as dictionary keys'], ['`upper()`', '`isalpha()`', '`capitalize()`', '`startswith()`']],
-            points=[4.0, 4.0],
+            points=[2.0, 2.0],
             grade=['parts'],
         )
